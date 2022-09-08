651da06c73757bc36323101f3b3112b6922cd1ae
Michael Camp <camp@email.arizona.edu>
2022-09-08 07:51:58 -0700

    Built a UA navbar component

---
+++ azlibrary_react/src/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <div class="bg-red p-3 mb-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12"><span class="text-uppercase heading-style m-0 text-white">The University of
+                        Arizona</span></div>
+                </div>
+            </div>
+        </div>
+    )
+}
