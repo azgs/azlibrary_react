d705b0a8b54052a3f276abf6f9387b089a4b8357
Michael Camp <camp@email.arizona.edu>
2022-09-08 12:31:15 -0700

    Sorting CollectionGroups by Abbreviation

--- azlibrary_react/src/SelectCollectionGroup.js
+++ azlibrary_react/src/SelectCollectionGroup.js
@@ -6,10 +6,21 @@ export default class SelectCollectionGroup extends React.Component {
         groups: [],
     }
 
+    sortByAbbreviation(){  
+        return function(a,b){  
+           if(a["abbrv"] > b["abbrv"])  
+              return 1;  
+           else if(a["abbrv"] < b["abbrv"])  
+              return -1;  
+       
+           return 0;  
+        }  
+     }
+
     componentDidMount() {
         axios.get(`https://devdata.azgs.arizona.edu/api/v1/dicts/collection_groups`)
             .then(res => {
-                const groups = res.data.data;
+                const groups = res.data.data.sort(this.sortByAbbreviation());
                 this.setState({ groups });
             })
     }
@@ -23,7 +34,7 @@ export default class SelectCollectionGroup extends React.Component {
                     {
                         this.state.groups
                             .map(group =>
-                                <option key={group.id} value={group.abbrv}>{group.name} ({group.abbrv})</option>
+                                <option key={group.id} value={group.abbrv}>{group.abbrv} - {group.name}</option>
                             )
                     }
                 </select>
