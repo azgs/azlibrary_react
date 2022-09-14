ead7ea6c0a7f9ed78313aa9e1d316aef3b2ecf4d
Michael Camp <camp@email.arizona.edu>
2022-09-14 09:27:14 -0700

    Added a link to the API help in nav

--- azlibrary_react/src/NavBar.js
+++ azlibrary_react/src/NavBar.js
@@ -3,10 +3,12 @@ import React from 'react'
 export default function NavBar() {
     return (
         <div className="bg-red p-3 mb-5">
-            <div className="container">
+            <div className="container-fluid">
                 <div className="row">
                     <div className="col-12"><span className="text-uppercase heading-style m-0 text-white">The University of
-                        Arizona</span></div>
+                        Arizona</span>
+                        <a className="pull-right" target="_blank" rel="noopener noreferrer" href="https://devdata.azgs.arizona.edu/api/v1/metadata?help=html">API</a>
+                    </div>
                 </div>
             </div>
         </div>
