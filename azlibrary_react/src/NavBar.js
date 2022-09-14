8d19bebe992c4fa8cf48f3d4cb5620720a5f1083
Michael Camp <camp@email.arizona.edu>
2022-09-14 14:04:30 -0700

    class to className

--- azlibrary_react/src/NavBar.js
+++ azlibrary_react/src/NavBar.js
@@ -13,12 +13,12 @@ export default function NavBar() {
                 </div>
             </header>
 
-            <ul class="nav justify-content-end">
-                <li class="nav-item">
-                    <a class="nav-link" href="https://digital.arizona.edu/arizona-bootstrap/docs/2.0/getting-started/introduction/" target="_blank" rel="noopener noreferrer">AZBootstrap</a>
+            <ul className="nav justify-content-end">
+                <li className="nav-item">
+                    <a className="nav-link" href="https://digital.arizona.edu/arizona-bootstrap/docs/2.0/getting-started/introduction/" target="_blank" rel="noopener noreferrer">AZBootstrap</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="https://devdata.azgs.arizona.edu/api/v1/metadata?help=html" target="_blank" rel="noopener noreferrer">API</a>
+                <li className="nav-item">
+                    <a className="nav-link" href="https://devdata.azgs.arizona.edu/api/v1/metadata?help=html" target="_blank" rel="noopener noreferrer">API</a>
                 </li>
             </ul>
         </>
