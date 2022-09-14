0e3e454caa298a030fe87686ba74930908a2a14e
Michael Camp <camp@email.arizona.edu>
2022-09-14 13:48:37 -0700

    UA Logo and links

--- azlibrary_react/src/NavBar.js
+++ azlibrary_react/src/NavBar.js
@@ -2,15 +2,25 @@ import React from 'react'
 
 export default function NavBar() {
     return (
-        <div className="bg-red p-3 mb-5">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-12"><span className="text-uppercase heading-style m-0 text-white">The University of
-                        Arizona</span>
-                        <a className="pull-right" target="_blank" rel="noopener noreferrer" href="https://devdata.azgs.arizona.edu/api/v1/metadata?help=html">API</a>
+        <>
+            <header className="bg-red arizona-header" id="header_arizona">
+                <div className="container">
+                    <div className="row">
+                        <a className="arizona-logo" href="http://www.arizona.edu" title="The University of Arizona homepage">
+                            <img className="arizona-line-logo" alt="The University of Arizona Wordmark Line Logo White" src="https://cdn.digital.arizona.edu/logos/v1.0.0/ua_wordmark_line_logo_white_rgb.min.svg" />
+                        </a>
                     </div>
                 </div>
-            </div>
-        </div>
+            </header>
+
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link" href="https://digital.arizona.edu/arizona-bootstrap/docs/2.0/getting-started/introduction/" target="_blank" rel="noopener noreferrer">AZBootstrap</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://devdata.azgs.arizona.edu/api/v1/metadata?help=html" target="_blank" rel="noopener noreferrer">API</a>
+                </li>
+            </ul>
+        </>
     )
 }
