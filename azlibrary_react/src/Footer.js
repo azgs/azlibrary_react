17fcea805e407433cef68cde919146100b914e1d
Michael Camp <camp@email.arizona.edu>
2022-09-15 11:42:13 -0700

    added noreferrer

--- azlibrary_react/src/Footer.js
+++ azlibrary_react/src/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
                         <p>520.621.2352 | 1955 East 6th Street, P.O. Box 210184, Tucson, AZ 85721</p>
                         <p>The maps, reports, and other information and content on this website are provided as a public service for informational purposes only.  Accuracy is not guaranteed, and the information contained or linked on this website should not be relied on except as general information.  The University makes no warranties or representations of any kind, and specifically disclaims all warranties including the warranty of merchantability and fitness for a particular purpose.  Users are responsible for verification of all facts and information to their own satisfaction.</p>
                         <hr />
-                        <p>&copy; 2022 The Arizona Board of Regents on behalf of <a href="https://www.arizona.edu" target="_blank">The University of Arizona</a>.</p>
+                        <p>&copy; 2022 The Arizona Board of Regents on behalf of <a href="https://www.arizona.edu" target="_blank" rel="noopener noreferrer" >The University of Arizona</a>.</p>
                     </div>
                 </div>
             </div>
