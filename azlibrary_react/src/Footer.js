4a80b085a62d55edbbd82c54545538746e70013b
Michael Camp <camp@email.arizona.edu>
2022-09-16 15:05:39 -0700

    Latest AZ bootstrap

--- azlibrary_react/src/Footer.js
+++ azlibrary_react/src/Footer.js
@@ -5,9 +5,10 @@ export default function Footer() {
         <footer className="footer bg-warm-gray text-center mt-4">
             <div className="container py-4">
                 <div className="row">
-                    <div className="col-12 small">
+                    <div className="col-12">
                         <p>520.621.2352 | 1955 East 6th Street, P.O. Box 210184, Tucson, AZ 85721</p>
-                        <p>The maps, reports, and other information and content on this website are provided as a public service for informational purposes only.  Accuracy is not guaranteed, and the information contained or linked on this website should not be relied on except as general information.  The University makes no warranties or representations of any kind, and specifically disclaims all warranties including the warranty of merchantability and fitness for a particular purpose.  Users are responsible for verification of all facts and information to their own satisfaction.</p>
+                        <p><a href="https://www.arizona.edu/information-security-privacy" target="_blank" rel="noopener noreferrer">University Information Security and Privacy</a></p>
+                        <p className='small'>The maps, reports, and other information and content on this website are provided as a public service for informational purposes only.  Accuracy is not guaranteed, and the information contained or linked on this website should not be relied on except as general information.  The University makes no warranties or representations of any kind, and specifically disclaims all warranties including the warranty of merchantability and fitness for a particular purpose.  Users are responsible for verification of all facts and information to their own satisfaction.</p>
                         <hr />
                         <p>&copy; 2022 The Arizona Board of Regents on behalf of <a href="https://www.arizona.edu" target="_blank" rel="noopener noreferrer" >The University of Arizona</a>.</p>
                     </div>
