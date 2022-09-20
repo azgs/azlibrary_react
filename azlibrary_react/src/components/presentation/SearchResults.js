13868c01c682a7426f05e9e3ca26f93106b34ef4
Michael Camp <camp@email.arizona.edu>
2022-09-20 13:26:43 -0700

    Switched from class to function component

--- azlibrary_react/src/components/presentation/SearchResults.js
+++ azlibrary_react/src/components/presentation/SearchResults.js
@@ -1,60 +1,61 @@
-import React from 'react';
+import React from 'react'
 
-export default class SearchResults extends React.Component {
+export default function SearchResults({ results, getResults, apiError }) {
+    return (
+        <div className="container-fluid">
 
-    render() {
-        return (
-            <div className="container-fluid">
+            {apiError && <div className="alert alert-danger text-center font-weight-bold" role="alert">
+                {apiError}
+            </div>}
 
-                {this.props.apiError && <div className="alert alert-danger text-center font-weight-bold" role="alert">
-                    {this.props.apiError}
-                </div>}
+            {
+                results?.data?.map(result =>
 
-                {
-                    this.props.results?.data?.map(result =>
-
-                        <div key={result.collection_id} className="card mb-3">
-                            <div className="card-header">
-                                <a className='searchUrl' target="_blank" rel="noopener noreferrer" href={"https://devdata.azgs.arizona.edu/api/v1/metadata?collection_id=" + result.collection_id}>{result.metadata.title}</a>
-                            </div>
-                            <div className="card-body">
-                                <p>
-                                    <strong>Author:</strong>
-                                    {result.metadata.authors.map(author =>
-                                        <div key={author.person}>{author.person}</div>
-                                    )
-                                    }
-                                </p>
-
-                                <p className="card-text"><strong>Abstract:</strong> {result.metadata.abstract}</p>
-
-                                <strong>Links:</strong>
-                                <ul className='ul-triangles'>
-                                    {result.links.map(link =>
-                                        <li key={link.rel}><a href={link.href} className="card-link">{link.rel}</a></li>
-                                    )
-                                    }
-                                </ul>
-                            </div>
+                    <div key={result.collection_id} className="card mb-3">
+                        <div className="card-header">
+                            <a target="_blank" rel="noopener noreferrer" href={"https://devdata.azgs.arizona.edu/api/v1/metadata?collection_id=" + result.collection_id}>{result.metadata.title}</a>
                         </div>
-                    )
-                }
+                        <div className="card-body">
 
-                <div>
-                    <nav aria-label="Page navigation example">
-                        <ul className="pagination justify-content-end">
-                            {
-                                this.props.results?.links?.map(link =>
-                                    <li key={link.rel} className="page-item">
-                                        <button className="page-link" onClick={() => this.props.getResults(link.href)}>{link.rel}</button>
-                                    </li>
+                            <div>
+                                <strong>Year:</strong> {result.metadata.year}
+                            </div>
+
+                            <div>
+                                <strong>Author:</strong>
+                                {result.metadata.authors.map(author =>
+                                    <div key={author.person}>{author.person}</div>
                                 )
-                            }
-                        </ul>
-                    </nav>
-                </div>
+                                }
+                            </div>
 
+                            <div className="card-text"><strong>Abstract:</strong> {result.metadata.abstract}</div>
+
+                            <strong>Links:</strong>
+                            <ul className='ul-triangles'>
+                                {result.links.map(link =>
+                                    <li key={link.rel}><a href={link.href} className="card-link">{link.rel}</a></li>
+                                )
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                )
+            }
+
+            <div>
+                <nav aria-label="Page navigation example">
+                    <ul className="pagination justify-content-end">
+                        {
+                            results?.links?.map(link =>
+                                <li key={link.rel} className="page-item">
+                                    <button className="page-link" onClick={() => getResults(link.href)}>{link.rel}</button>
+                                </li>
+                            )
+                        }
+                    </ul>
+                </nav>
             </div>
-        )
-    }
+        </div>
+    )
 }
