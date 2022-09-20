41cb8fb75225aff61f438dbc29bb2d65c911d1a3
Michael Camp <camp@email.arizona.edu>
2022-09-20 09:05:10 -0700

    replaced table with bootstrap cards

--- azlibrary_react/src/SearchResults.js
+++ azlibrary_react/src/SearchResults.js
@@ -12,27 +12,21 @@ export default class SearchResults extends React.Component {
                     {this.props.apiError}
                 </div>}
 
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>year</th>
-                            <th>collection_id</th>
-                            <th>title</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.props.results?.data?.map(result =>
-                                <tr key={result.collection_id}>
-                                    <td>{result.metadata.year}</td>
-                                    <td><a href={result.links[0].href}>{result.collection_id}</a></td>
-                                    <td>{result.metadata.title}</td>
-                                </tr>
-                            )
-                        }
-                    </tbody>
+                {
+                    this.props.results?.data?.map(result =>
 
-                </table>
+                        <div className="card mb-3">
+                            <div className="card-header">
+                                {result.metadata.title}
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">{result.collection_id}</h5>
+                                <p className="card-text">{result.metadata.abstract}</p>
+                                <a href={result.links[0].href} className="btn btn-red">Download</a>
+                            </div>
+                        </div>
+                    )
+                }
 
                 <div>
                     <nav aria-label="Page navigation example">
