70ee12209f53f382476878dab402a9755dddc3f8
Michael Camp <camp@email.arizona.edu>
2022-09-30 10:58:56 -0700

    Update Paging.js

--- azlibrary_react/src/components/presentation/Paging.js
+++ azlibrary_react/src/components/presentation/Paging.js
@@ -2,15 +2,16 @@ export default function Paging({ links, setSearchUrl }) {
 
     const self = links?.find((link) => link.rel === 'self');
     const first = links?.find((link) => link.rel === 'first');
+    const previous = links?.find((link) => link.rel === 'previous');
     const next = links?.find((link) => link.rel === 'next');
     const last = links?.find((link) => link.rel === 'last');
 
     const params = new URLSearchParams(last?.href)
     const offset = params.get("offset");
 
-    const foo = self?.href === last?.href ? "disabled" : "";
-
-    console.log(foo);
+    console.log(self?.href);
+    console.log(previous?.href);
+    console.log(self?.href !== previous?.href);
 
     return (
         // <div>
@@ -29,13 +30,13 @@ export default function Paging({ links, setSearchUrl }) {
 
         <nav aria-label="Page navigation example">
             <ul className="pagination justify-content-end">
-                <li className={`page-item ${self?.href === last?.href ? "disabled" : ""}`}  >
-                    <button className="page-link" onClick={() => setSearchUrl(last?.href)}>Previous</button>
+                <li className={`page-item ${previous ? "" : "disabled"}`}>
+                    <button className="page-link" onClick={() => setSearchUrl(previous?.href)}>Previous</button>
                 </li>
                 {/* <li className="page-item"><button className="page-link">1</button></li>
                 <li className="page-item"><button className="page-link">2</button></li>
                 <li className="page-item"><button className="page-link">3</button></li> */}
-                <li className="page-item">
+                <li className={`page-item ${next ? "" : "disabled"}`}>
                     <button className="page-link" onClick={() => setSearchUrl(next?.href)}>Next</button>
                 </li>
             </ul>
