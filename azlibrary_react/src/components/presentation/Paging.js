e5c62b8ae03e6b4a34e4d093af112c1cabf2ef8a
Michael Camp <camp@email.arizona.edu>
2022-09-30 11:00:22 -0700

    Pulling data from prod

--- azlibrary_react/src/components/presentation/Paging.js
+++ azlibrary_react/src/components/presentation/Paging.js
@@ -1,33 +1,15 @@
 export default function Paging({ links, setSearchUrl }) {
 
-    const self = links?.find((link) => link.rel === 'self');
-    const first = links?.find((link) => link.rel === 'first');
+    // const self = links?.find((link) => link.rel === 'self');
+    // const first = links?.find((link) => link.rel === 'first');
     const previous = links?.find((link) => link.rel === 'previous');
     const next = links?.find((link) => link.rel === 'next');
-    const last = links?.find((link) => link.rel === 'last');
+    // const last = links?.find((link) => link.rel === 'last');
 
-    const params = new URLSearchParams(last?.href)
-    const offset = params.get("offset");
-
-    console.log(self?.href);
-    console.log(previous?.href);
-    console.log(self?.href !== previous?.href);
+    // const params = new URLSearchParams(last?.href)
+    // const offset = params.get("offset");
 
     return (
-        // <div>
-        //     <nav aria-label="Page navigation example">
-        //         <ul className="pagination justify-content-end">
-        //             {
-        //                links?.map(link =>
-        //                     <li key={link.rel} className="page-item">
-        //                         <button className="page-link" onClick={() => setSearchUrl(link.href)}>{link.rel}</button>
-        //                     </li>
-        //                 )
-        //             }
-        //         </ul>
-        //     </nav>
-        // </div>
-
         <nav aria-label="Page navigation example">
             <ul className="pagination justify-content-end">
                 <li className={`page-item ${previous ? "" : "disabled"}`}>
