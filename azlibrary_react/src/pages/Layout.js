320a330eaf376bffb1f9a632a5a51f7285eaf459
Michael Camp <camp@email.arizona.edu>
2022-09-30 12:09:08 -0700

    responsive UA logo - still needs tweaking

--- azlibrary_react/src/pages/Layout.js
+++ azlibrary_react/src/pages/Layout.js
@@ -7,8 +7,8 @@ export default function NavBar() {
             <header className="bg-red arizona-header" id="header_arizona">
                 <div className="container-fluid">
                     <div className="row">
-                        <a className="arizona-logo" href="http://www.arizona.edu" title="The University of Arizona homepage">
-                            <img className="arizona-line-logo" alt="The University of Arizona Wordmark Line Logo White" src="https://cdn.digital.arizona.edu/logos/v1.0.0/ua_wordmark_line_logo_white_rgb.min.svg" />
+                        <a className="arizona-logo" href="http://www.arizona.edu" title="The University of Arizona">
+                            <img className="img-fluid" alt="The University of Arizona Wordmark Line Logo White" src="https://cdn.digital.arizona.edu/logos/v1.0.0/ua_wordmark_line_logo_white_rgb.min.svg" />
                         </a>
                     </div>
                 </div>
