d992d9b2915925f37cce64602381c36c45bbae6e
Michael Camp <camp@email.arizona.edu>
2022-09-30 13:24:43 -0700

    Display item as a single column

--- azlibrary_react/src/pages/Item.js
+++ azlibrary_react/src/pages/Item.js
@@ -32,106 +32,95 @@ export default function Item() {
 
   return (
 
-    <div className="container-fluid">
+    <>
+      <div className="container-fluid">
+        <Breadcrumb isItem={true} />
+      </div>
 
-      <Breadcrumb isItem={true} />
+      <div className="container">
 
-      {error && <div className="alert alert-danger text-center font-weight-bold" role="alert">
-        🐟collection not found🐟
-      </div>}
+        {error && <div className="alert alert-danger text-center font-weight-bold" role="alert">
+          🐟collection not found🐟
+        </div>}
 
-      {collection && <div className="row">
-
-        <div className="col-xl-3 col-lg-4 mb-2">
-          <MapContainer bounds={bounds} boundsOptions={{ padding: [100, 100] }} >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Rectangle bounds={bounds} pathOptions={{ color: 'blue' }} />
-          </MapContainer>
-        </div>
-
-        <div className="col-xl-9 col-lg-8">
+        {collection && <div>
 
           <h3 className="text-center mt-0">{collection.metadata.title} {collection.metadata.private && <span className="badge badge-warning">Private</span>}</h3>
+
+          <div className="">
+            <MapContainer bounds={bounds} boundsOptions={{ padding: [100, 100] }} >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Rectangle bounds={bounds} pathOptions={{ color: 'blue' }} />
+            </MapContainer>
+          </div>
 
           <hr />
 
           <dl className="row">
 
             {collection.metadata.informal_name && <>
-              <dt className="col-sm-2 text-md-right">Informal Name</dt>
+              <dt className="col-sm-2">Informal Name</dt>
               <dd className="col-sm-10">{collection.metadata.informal_name}</dd>
             </>}
 
-            <dt className="col-sm-2 text-md-right">Year</dt>
+            <dt className="col-sm-2">Year</dt>
             <dd className="col-sm-10">{collection.metadata.year}</dd>
 
-            <dt className="col-sm-2 text-md-right">Collection Group</dt>
+            <dt className="col-sm-2">Collection Group</dt>
             <dd className="col-sm-10">{collection.metadata.collection_group.name}</dd>
 
 
             {collection.metadata.series && <>
-              <dt className="col-sm-2 text-md-right">Series</dt>
-            <dd className="col-sm-10">{collection.metadata.series}</dd>
+              <dt className="col-sm-2">Series</dt>
+              <dd className="col-sm-10">{collection.metadata.series}</dd>
             </>}
 
-            <dt className="col-sm-2 text-md-right">Author{collection.metadata.authors.length === 1 ? "" : "s"}</dt>
+            <dt className="col-sm-2">Author{collection.metadata.authors.length === 1 ? "" : "s"}</dt>
             <dd className="col-sm-10">
               {collection.metadata.authors.map(author =>
                 <div key={author.person}>{author.person}</div>
               )}
             </dd>
 
-            <dt className="col-sm-2 text-md-right">Abstract</dt>
+            <dt className="col-sm-2">Abstract</dt>
             <dd className="col-sm-10">{collection.metadata.abstract}</dd>
 
             {collection.metadata.license.url && collection.metadata.license.type && <>
-              <dt className="col-sm-2 text-md-right">License</dt>
+              <dt className="col-sm-2">License</dt>
               <dd className="col-sm-10"><a href={collection.metadata.license.url} target="_blank" rel="noopener noreferrer">{collection.metadata.license.type}</a></dd>
             </>
             }
 
-            <dt className="col-sm-2 text-md-right">Language</dt>
+            <dt className="col-sm-2">Language</dt>
             <dd className="col-sm-10">{collection.metadata.language}</dd>
 
-            <dt className="col-sm-2 text-md-right">Keyword{collection.metadata.keywords.length === 1 ? "" : "s"}</dt>
+            <dt className="col-sm-2">Keyword{collection.metadata.keywords.length === 1 ? "" : "s"}</dt>
             <dd className="col-sm-10 split-sm-3-col">
               {collection.metadata.keywords.map(keyword =>
                 <div key={keyword.name}>{keyword.name} ({keyword.type})</div>
               )}
             </dd>
 
-            <dt className="col-sm-2 text-md-right">File{collection.metadata.files.length === 1 ? "" : "s"}</dt>
+            <dt className="col-sm-2">File{collection.metadata.files.length === 1 ? "" : "s"}</dt>
             <dd className="col-sm-10">
               {collection.metadata.files.map(file =>
                 <div key={file.name}>{file.name} ({file.type})</div>
               )}
 
               {collection.links.map(link =>
-                <div key={link.rel}><a className="btn btn-blue btn-sm" title={link.rel === "describes" ? "Download" : link.rel} href={link.href.replace("http://","https://")}>{link.rel === "describes" ? "Download" : link.rel}</a></div>
+                <div key={link.rel}><a className="btn btn-blue btn-sm" title={link.rel === "describes" ? "Download" : link.rel} href={link.href}>{link.rel === "describes" ? "Download" : link.rel}</a></div>
               )}
             </dd>
 
-
-
           </dl>
-
-          {/* <div>
-            {collection.links.map(link =>
-              <div key={link.rel}><a className="btn btn-blue btn-sm" title={link.rel === "describes" ? "Download" : link.rel} href={link.href}>{link.rel === "describes" ? "Download" : link.rel}</a></div>
-            )}
-          </div> */}
 
           {/* <code>{JSON.stringify(collection.metadata)}</code> */}
 
         </div>
-
+        }
       </div>
-
-      }
-
-    </div>
-
+    </>
   )
 }
