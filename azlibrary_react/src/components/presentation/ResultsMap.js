89936287bc3f667b97298789c9e9a84c352ebed9
Michael Camp <camp@email.arizona.edu>
2022-10-20 14:38:53 -0700

    Printing WKT poly on init and moveend

--- azlibrary_react/src/components/presentation/ResultsMap.js
+++ azlibrary_react/src/components/presentation/ResultsMap.js
@@ -1,18 +1,27 @@
-import { useMapEvent, MapContainer, TileLayer, Tooltip, Rectangle } from 'react-leaflet'
+import { useMap, useMapEvent, MapContainer, TileLayer, Tooltip, Rectangle } from 'react-leaflet'
 
 export default function ResultsMap({ results, highlightBox }) {
 
-    function MyComponent() {
-        const map = useMapEvent('moveend', () => {
-            const bounds = map.getBounds();
-            const southWest = bounds.getSouthWest();
-            const northEast = bounds.getNorthEast();
-            const northWest = bounds.getNorthWest();
-            const southEast = bounds.getSouthEast();
+    function printWKT(map) {
+        const bounds = map.getBounds();
+        const southWest = bounds.getSouthWest();
+        const northEast = bounds.getNorthEast();
+        const northWest = bounds.getNorthWest();
+        const southEast = bounds.getSouthEast();
 
-            console.log(`POLYGON(${northWest.lng} ${northWest.lat}, ${southWest.lng} ${southWest.lat}, ${southEast.lng} ${southEast.lat}, ${northEast.lng} ${northEast.lat})`);
+        console.log(`POLYGON(${northWest.lng} ${northWest.lat}, ${southWest.lng} ${southWest.lat}, ${southEast.lng} ${southEast.lat}, ${northEast.lng} ${northEast.lat})`);
+    }
+
+    function InitializeGeom() {
+        const map = useMap();
+        printWKT(map);
+    }
+
+    function UpdateGeom() {
+        const map = useMapEvent('moveend', () => {
+            printWKT(map);
         })
-        return null
+        return null;
     }
 
     return (
@@ -20,26 +29,26 @@ export default function ResultsMap({ results, highlightBox }) {
             <div className='mb-2'>
                 {results && <MapContainer center={[34.16, -111.62]} zoom={6} scrollWheelZoom={false} >
 
+                    <InitializeGeom />
+
+                    <UpdateGeom />
+
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
 
-                    {
-                        highlightBox && <Rectangle key={highlightBox.id} eventHandlers={{ click: () => window.location.href = "item/" + highlightBox.id }} bounds={[[highlightBox.bbox.north, highlightBox.bbox.east], [highlightBox.bbox.south, highlightBox.bbox.west]]} pathOptions={{ color: "#ff0000" }}>
-                            <Tooltip>{highlightBox.title}</Tooltip>
+                    {highlightBox && <Rectangle key={highlightBox.id} eventHandlers={{ click: () => window.location.href = "item/" + highlightBox.id }} bounds={[[highlightBox.bbox.north, highlightBox.bbox.east], [highlightBox.bbox.south, highlightBox.bbox.west]]} pathOptions={{ color: "#ff0000" }}>
+                        <Tooltip>{highlightBox.title}</Tooltip>
+                    </Rectangle>
+                    }
+
+                    {results.map(result =>
+                        <Rectangle key={result.id} eventHandlers={{ click: () => window.location.href = "item/" + result.id }} bounds={[[result.bbox.north, result.bbox.east], [result.bbox.south, result.bbox.west]]} pathOptions={{ color: "#1E5288" }}>
+                            <Tooltip>{result.title}</Tooltip>
                         </Rectangle>
+                    )
                     }
-
-                    {
-                        results.map(result =>
-                            <Rectangle key={result.id} eventHandlers={{ click: () => window.location.href = "item/" + result.id }} bounds={[[result.bbox.north, result.bbox.east], [result.bbox.south, result.bbox.west]]} pathOptions={{ color: "#1E5288" }}>
-                                <Tooltip>{result.title}</Tooltip>
-                            </Rectangle>
-                        )
-                    }
-
-                    <MyComponent />
 
                 </MapContainer>}
             </div>
