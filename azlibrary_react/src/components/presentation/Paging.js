c9c7cc73625c7f4e1a0c186208b0f9551c86a212
Michael Camp <camp@email.arizona.edu>
2022-11-01 15:08:17 -0700

    cleanup

--- azlibrary_react/src/components/presentation/Paging.js
+++ azlibrary_react/src/components/presentation/Paging.js
@@ -8,27 +8,32 @@ export default function Paging({ links, limit, offset, setOffset }) {
         return link;
     });
 
+    // Dictionary from API links
+    const apiLinks = Object.fromEntries(links.map(({ rel, href }) => ([rel, getOffsetFromUrl(href)])));
+
     // offset or default
     offset = offset ? offset : 0;
 
     // limit or default
     limit = limit ? limit : 10;
 
-    // Dictionary from API links
-    const apiLinks = Object.fromEntries(links.map(({ rel, href }) => ([rel, getOffsetFromUrl(href)])));
+    // last offset or default
+    const maxOffset = apiLinks['last'] ?? 0;
+
     const isFirst = apiLinks['self'] === apiLinks['first'];
     const isLast = apiLinks['self'] === apiLinks['last'];
-    const maxOffset = apiLinks['last'] ?? 0;
 
     const navLinks = [];
 
     const startOffset = Math.max(0, offset - (4 * limit));
-    const endOffset = Math.min(maxOffset, offset + (4 * limit));
+
+    const endOffset = Math.min(maxOffset, startOffset + (8 * limit));
 
     for (let i = startOffset; i <= endOffset; i = i + limit) {
         navLinks.push({pageNumber: (((i % (maxOffset + limit)) / limit) + 1), offset: i, active: (i === offset)});
     }
 
+    // Returns the offset paramter from a link
     function getOffsetFromUrl(url) {
         const params = new URLSearchParams(url)
 
