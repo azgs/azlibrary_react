f3cd283a44c2645588c88e8566880f6df1fab509
Michael Camp <camp@email.arizona.edu>
2022-11-02 14:00:40 -0700

    Hide paging controls when 0 results

--- azlibrary_react/src/pages/Home.js
+++ azlibrary_react/src/pages/Home.js
@@ -25,6 +25,7 @@ export default function Home() {
   const [results, setResults] = useState([]);
   const [apiError, setApiError] = useState();
   const [boundingBoxes, setBoundingBoxes] = useState();
+  const [links, setLinks] = useState();
   const [highlightBox, setHighlightBox] = useState();
 
   // Fire API call whenever searchUrl updates
@@ -54,8 +55,10 @@ export default function Home() {
     };
   }, [searchUrl]);
 
-  // Grab bounding boxes from results
+  // Grab bounding boxes and links from results
   useEffect(() => {
+
+    setLinks(results?.links);
 
     const boxes = results?.data?.map(item => ({ id: item.collection_id, title: item.metadata.title, bbox: item.metadata.bounding_box }));
 
@@ -123,9 +126,9 @@ export default function Home() {
           </div>}
         </div>
 
-        {/* Paging */}
+        {/* Top Paging */}
         <div className="col-12">
-          {results.data?.length !== 0 && <Paging links={results?.links} limit={limit} offset={offset} setOffset={setOffset} />}
+          {links && results.data?.length !== 0 && <Paging links={links} limit={limit} offset={offset} setOffset={setOffset} />}
         </div>
 
         {/* Results map */}
@@ -138,9 +141,9 @@ export default function Home() {
           <SearchResults results={results} setHighlightBox={setHighlightBox} />
         </div>
 
-        {/* Paging */}
+        {/* Bottom Paging */}
         <div className="col-12">
-          {results.data?.length !== 0 && <Paging links={results?.links} limit={limit} offset={offset} setOffset={setOffset} />}
+          {links && results.data?.length !== 0 && <Paging links={links} limit={limit} offset={offset} setOffset={setOffset} />}
         </div>
 
       </div>
