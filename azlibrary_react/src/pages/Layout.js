c574fcc7b6ec4ef8b84a239f281d6e2baa0ab639
Michael Camp <camp@email.arizona.edu>
2022-11-28 08:48:13 -0700

    Added "under development" banner

--- azlibrary_react/src/pages/Layout.js
+++ azlibrary_react/src/pages/Layout.js
@@ -7,7 +7,7 @@ export default function NavBar() {
     return (
         <>
             <header className="bg-red arizona-header" id="header_arizona">
-                <div className="container-fluid">
+                <div className="container">
                     <div className="row">
                         <a className="arizona-logo" href="http://www.arizona.edu" title="The University of Arizona">
                             <img className="arizona-line-logo" alt="The University of Arizona Wordmark Line Logo White" src="https://cdn.digital.arizona.edu/logos/v1.0.0/ua_wordmark_line_logo_white_rgb.min.svg" />
@@ -16,33 +16,41 @@ export default function NavBar() {
                 </div>
             </header>
 
-            <nav className="navbar navbar-expand-lg navbar-light mb-4">
-                <a className="navbar-brand" href="https://azgs.arizona.edu/"><img src={logo} alt="AZGS logo" className="img-fluid" /></a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="icon material-icons-sharp"> menu </span>
-                </button>
+            <div className="container">
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">Home</Link>
-                        </li>
+                <nav className="navbar navbar-expand-lg navbar-light mb-4">
+                    <a className="navbar-brand" href="https://azgs.arizona.edu/"><img src={logo} alt="AZGS logo" className="img-fluid" /></a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="icon material-icons-sharp"> menu </span>
+                    </button>
 
-                        {debug && <>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto">
                             <li className="nav-item">
-                                <a className="nav-link" href="https://devdata.azgs.arizona.edu/api/v1/metadata?help=html" target="_blank" rel="noopener noreferrer">API</a>
+                                <Link className="nav-link" to="/">Home</Link>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="https://xdd.wisc.edu/adept/" target="_blank" rel="noopener noreferrer">Adept</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="https://digital.arizona.edu/arizona-bootstrap/docs/2.0/getting-started/introduction/" target="_blank" rel="noopener noreferrer">AZBootstrap</a>
-                            </li>
-                        </>}
 
-                    </ul>
+                            {debug && <>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="https://devdata.azgs.arizona.edu/api/v1/metadata?help=html" target="_blank" rel="noopener noreferrer">API</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="https://xdd.wisc.edu/adept/" target="_blank" rel="noopener noreferrer">Adept</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="https://digital.arizona.edu/arizona-bootstrap/docs/2.0/getting-started/introduction/" target="_blank" rel="noopener noreferrer">AZBootstrap</a>
+                                </li>
+                            </>}
+
+                        </ul>
+                    </div>
+                </nav>
+
+                <div className="alert alert-success" role="alert">
+                    This website is still under development. Please submit any issues to <a className="alert-link" href="https://github.com/azgs/azlibrary_react/issues" target="_blank" rel="noopener noreferrer" >GitHub</a> 
                 </div>
-            </nav>
+
+            </div>
 
             <div className='fullpage'>
                 <Outlet />
