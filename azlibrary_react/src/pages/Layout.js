2031bef85e6d9a46196cb901232db007af9a2610
Michael Camp <camp@email.arizona.edu>
2022-12-01 15:50:12 -0700

    Added padding to the banner

--- azlibrary_react/src/pages/Layout.js
+++ azlibrary_react/src/pages/Layout.js
@@ -46,8 +46,8 @@ export default function NavBar() {
                     </div>
                 </nav>
 
-                <div className="alert alert-success text-center" role="alert">
-                    The AZGS Library website is currently in development. Please submit any issues to <a className="alert-link" href="https://github.com/azgs/azlibrary_react/issues" target="_blank" rel="noopener noreferrer" >GitHub.</a> 
+                <div className="alert alert-success text-center rounded py-4" role="alert">
+                    The AZGS Library website is currently under development. Please submit any issues to <a className="alert-link" href="https://github.com/azgs/azlibrary_react/issues" target="_blank" rel="noopener noreferrer" >GitHub.</a> 
                 </div>
 
             </div>
