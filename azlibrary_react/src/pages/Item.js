75e363dbe78e39105933878b5017b58f4fd0256d
Michael Camp <camp@email.arizona.edu>
2022-12-01 10:10:52 -0700

    Added permalink to UA Library

--- azlibrary_react/src/pages/Item.js
+++ azlibrary_react/src/pages/Item.js
@@ -11,6 +11,7 @@ export default function Item() {
   const [collection, setCollection] = useState();
   const [error, setError] = useState();
   const [bounds, setBounds] = useState();
+  const [uaLibraryLink, setUaLibraryLink] = useState();
 
   useEffect(() => {
 
@@ -18,10 +19,14 @@ export default function Item() {
       try {
         const res = await azgsApi.get('/metadata/' + collectionId);
         const json = res.data.data;
+
         setCollection(json);
 
         const boundBox = [[json.metadata.bounding_box.north, json.metadata.bounding_box.east], [json.metadata.bounding_box.south, json.metadata.bounding_box.west]];
         setBounds(boundBox);
+
+        setUaLibraryLink(json.metadata.links.find(x => x.name === 'UA Library'));
+
       } catch (error) {
         setError(error);
       }
@@ -33,98 +38,104 @@ export default function Item() {
 
   return (
 
-      <div className="container">
+    <div className="container">
 
-        <Breadcrumb isItem={true} />
+      <Breadcrumb isItem={true} />
 
-        {error && <div className="alert alert-danger text-center font-weight-bold" role="alert">
-          🐟 Collection not found. 🐟
-        </div>}
+      {error && <div className="alert alert-danger text-center font-weight-bold" role="alert">
+        🐟 Collection not found. 🐟
+      </div>}
 
-        {collection && <div>
+      {collection && <div>
 
-          <h3 className="text-center mt-0">{collection.metadata.title} {collection.metadata.private && <span className="badge badge-warning">Private</span>}</h3>
+        <h3 className="text-center mt-0">{collection.metadata.title} {collection.metadata.private && <span className="badge badge-warning">Private</span>}</h3>
 
-          <div className="">
-            <MapContainer bounds={bounds} boundsOptions={{ padding: [100, 100] }} >
-              <TileLayer
-                attribution='<a href="https://usgs.gov/">U.S. Geological Survey</a>' url="https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}"
-              />
-              <Rectangle bounds={bounds} pathOptions={{ color: 'blue' }} />
-            </MapContainer>
-          </div>
-
-          <hr />
-
-          <dl className="row">
-
-            {collection.metadata.informal_name && <>
-              <dt className="col-sm-2">Informal Name</dt>
-              <dd className="col-sm-10">{collection.metadata.informal_name}</dd>
-            </>}
-
-            <dt className="col-sm-2">Year</dt>
-            <dd className="col-sm-10">{collection.metadata.year}</dd>
-
-            <dt className="col-sm-2">Collection Group</dt>
-            <dd className="col-sm-10">{collection.metadata.collection_group.name}</dd>
-
-
-            {collection.metadata.series && <>
-              <dt className="col-sm-2">Series</dt>
-              <dd className="col-sm-10">{collection.metadata.series}</dd>
-            </>}
-
-            {collection.metadata.identifiers.supersedes && <>
-              <dt className="col-sm-2">Supersedes</dt>
-              <dd className="col-sm-10"><Link className="" title={collection.metadata.identifiers.supersedes} to={"/item/" + collection.metadata.identifiers.supersedes}>{collection.metadata.identifiers.supersedes}</Link></dd>
-            </>}
-
-            {collection.metadata.identifiers.superseded_by && <>
-              <dt className="col-sm-2">Superseded By</dt>
-              <dd className="col-sm-10"><Link className="" title={collection.metadata.identifiers.superseded_by} to={"/item/" + collection.metadata.identifiers.superseded_by}>{collection.metadata.identifiers.superseded_by}</Link></dd>
-            </>}
-
-            <dt className="col-sm-2">Author{collection.metadata.authors.length === 1 ? "" : "s"}</dt>
-            <dd className="col-sm-10">
-              {collection.metadata.authors.map(author =>
-                <div key={author.person}>{author.person}</div>
-              )}
-            </dd>
-
-            <dt className="col-sm-2">Abstract</dt>
-            <dd className="col-sm-10">{collection.metadata.abstract}</dd>
-
-            {collection.metadata.license.url && collection.metadata.license.type && <>
-              <dt className="col-sm-2">License</dt>
-              <dd className="col-sm-10"><a href={collection.metadata.license.url} target="_blank" rel="noopener noreferrer">{collection.metadata.license.type}</a></dd>
-            </>
-            }
-
-            <dt className="col-sm-2">Language</dt>
-            <dd className="col-sm-10">{collection.metadata.language}</dd>
-
-            <dt className="col-sm-2">Keyword{collection.metadata.keywords.length === 1 ? "" : "s"}</dt>
-            <dd className="col-sm-10 split-sm-3-col">
-              {collection.metadata.keywords.map(keyword =>
-                <div key={keyword.name}>{keyword.name} ({keyword.type})</div>
-              )}
-            </dd>
-
-            <dt className="col-sm-2">File{collection.metadata.files.length === 1 ? "" : "s"}</dt>
-            <dd className="col-sm-10">
-              {collection.metadata.files.map(file =>
-                <div key={file.name}>{file.name} ({file.type})</div>
-              )}
-            </dd>
-          </dl>
-
-          <Downloads collectionId={collectionId} />
-
-          {/* <code>{JSON.stringify(collection.metadata)}</code> */}
-
+        <div className="">
+          <MapContainer bounds={bounds} boundsOptions={{ padding: [100, 100] }} >
+            <TileLayer
+              attribution='<a href="https://usgs.gov/">U.S. Geological Survey</a>' url="https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}"
+            />
+            <Rectangle bounds={bounds} pathOptions={{ color: 'blue' }} />
+          </MapContainer>
         </div>
-        }
+
+        <hr />
+
+        <dl className="row">
+
+          {collection.metadata.informal_name && <>
+            <dt className="col-sm-2">Informal Name</dt>
+            <dd className="col-sm-10">{collection.metadata.informal_name}</dd>
+          </>}
+
+          <dt className="col-sm-2">Year</dt>
+          <dd className="col-sm-10">{collection.metadata.year}</dd>
+
+          <dt className="col-sm-2">Collection Group</dt>
+          <dd className="col-sm-10">{collection.metadata.collection_group.name}</dd>
+
+
+          {collection.metadata.series && <>
+            <dt className="col-sm-2">Series</dt>
+            <dd className="col-sm-10">{collection.metadata.series}</dd>
+          </>}
+
+          {collection.metadata.identifiers.supersedes && <>
+            <dt className="col-sm-2">Supersedes</dt>
+            <dd className="col-sm-10"><Link className="" title={collection.metadata.identifiers.supersedes} to={"/item/" + collection.metadata.identifiers.supersedes}>{collection.metadata.identifiers.supersedes}</Link></dd>
+          </>}
+
+          {collection.metadata.identifiers.superseded_by && <>
+            <dt className="col-sm-2">Superseded By</dt>
+            <dd className="col-sm-10"><Link className="" title={collection.metadata.identifiers.superseded_by} to={"/item/" + collection.metadata.identifiers.superseded_by}>{collection.metadata.identifiers.superseded_by}</Link></dd>
+          </>}
+
+          <dt className="col-sm-2">Author{collection.metadata.authors.length === 1 ? "" : "s"}</dt>
+          <dd className="col-sm-10">
+            {collection.metadata.authors.map(author =>
+              <div key={author.person}>{author.person}</div>
+            )}
+          </dd>
+
+          <dt className="col-sm-2">Abstract</dt>
+          <dd className="col-sm-10">{collection.metadata.abstract}</dd>
+
+          {collection.metadata.license.url && collection.metadata.license.type && <>
+            <dt className="col-sm-2">License</dt>
+            <dd className="col-sm-10"><a href={collection.metadata.license.url} target="_blank" rel="noopener noreferrer">{collection.metadata.license.type}</a></dd>
+          </>
+          }
+
+          <dt className="col-sm-2">Language</dt>
+          <dd className="col-sm-10">{collection.metadata.language}</dd>
+
+          <dt className="col-sm-2">Keyword{collection.metadata.keywords.length === 1 ? "" : "s"}</dt>
+          <dd className="col-sm-10 split-sm-3-col">
+            {collection.metadata.keywords.map(keyword =>
+              <div key={keyword.name}>{keyword.name} ({keyword.type})</div>
+            )}
+          </dd>
+
+          {uaLibraryLink && <>
+            <dt className="col-sm-2">Permalink</dt>
+            <dd className="col-sm-10"><a href={uaLibraryLink.url} target="_blank" rel="noopener noreferrer">{uaLibraryLink.name}</a></dd>
+          </>
+          }
+
+          <dt className="col-sm-2">File{collection.metadata.files.length === 1 ? "" : "s"}</dt>
+          <dd className="col-sm-10">
+            {collection.metadata.files.map(file =>
+              <div key={file.name}>{file.name} ({file.type})</div>
+            )}
+          </dd>
+        </dl>
+
+        <Downloads collectionId={collectionId} />
+
+        {/* <code>{JSON.stringify(collection.metadata)}</code> */}
+
       </div>
+      }
+    </div>
   )
 }
