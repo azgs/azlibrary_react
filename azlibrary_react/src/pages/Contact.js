081282154991c4514623187ca3d5699ea50b9078
Michael Camp <camp@email.arizona.edu>
2022-12-05 14:39:52 -0700

    Added contact information

--- azlibrary_react/src/pages/Contact.js
+++ azlibrary_react/src/pages/Contact.js
@@ -1,6 +1,17 @@
 const Contact = () => {
-  return <div className="alert alert-info text-center" role="alert">
-    Contact Page
+  return <div className="container">
+    <h1>Contact AZGS</h1>
+    <dl>
+      <dt>Arizona Geological Survey</dt>
+      <dd>1955 East Sixth Street <br />
+        PO Box 210184</dd>
+      <dt>Phone</dt>
+      <dd>520-621-2470</dd>
+      <dt>Email</dt>
+      <dd><a href="mailto:azgs-info@arizona.edu">azgs-info@arizona.edu</a></dd>
+      <dt>GitHub Issues</dt>
+      <dd><a href="https://github.com/azgs/azlibrary_react/issues" target="_blank" rel="noopener noreferrer">https://github.com/azgs/azlibrary_react/issues</a></dd>
+    </dl>
   </div>;
 };
 
