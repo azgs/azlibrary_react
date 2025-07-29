fdf5658a3cd1e9375f1bba8fce0d1b0a15aa4eb9
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-07-29 15:35:43 -0700

    Add gisdata (all) to file type options

--- azlibrary_react/src/components/container/SelectFileType.js
+++ azlibrary_react/src/components/container/SelectFileType.js
@@ -4,16 +4,29 @@ import azgsApi from './AzgsApi';
 export default function SelectFileType({ id, className, fieldValue, onChange }) {
 
     //Don't really need to use state here, since we aren't hitting api to get these.But we might someday.
-    const [fileTypes, setFileTypes] = useState([
+    /*const [fileTypes, setFileTypes] = useState([
         "documents",
         "images",
         "notes",
         "metadata",
+        "gisdata",
         "gisdata:legacy",
         "gisdata:layers",
         "gisdata:raster",
         "gisdata:gems2",
         "gisdata:ncgmp09",
+    ]);*/
+    const [fileTypes, setFileTypes] = useState([
+        {display:"documents", value:"documents"},
+        {display:"images", value:"images"},
+        {display:"notes", value:"notes"},
+        {display:"metadata", value:"metadata"},
+        {display:"gisdata (all)", value:"gisdata"},
+        {display:"gisdata:legacy", value:"gisdata:legacy"},
+        {display:"gisdata:layers", value:"gisdata:layers"},
+        {display:"gisdata:raster", value:"gisdata:raster"},
+        {display:"gisdata:gems2", value:"gisdata:gems2"},
+        {display:"gisdata:ncgmp09", value:"gisdata:ncgmp09"},
     ]);
 
     return (
@@ -23,7 +36,7 @@ export default function SelectFileType({ id, className, fieldValue, onChange }) 
                 <option value="">--All File Types--</option>
                 {
                     fileTypes?.map((fileType, idx) =>
-                        <option key={idx} value={fileType}>{fileType}</option>
+                        <option key={idx} value={fileType.value}>{fileType.display}</option>
                     )
                 }
             </select>
