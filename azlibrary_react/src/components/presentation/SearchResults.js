6f0c2506dbaac0b064d8c368df7f35bde84bbfce
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-07-29 13:53:06 -0700

    Add No author specified message if that's the case

--- azlibrary_react/src/components/presentation/SearchResults.js
+++ azlibrary_react/src/components/presentation/SearchResults.js
@@ -11,7 +11,10 @@ export default function SearchResults({ results, setHighlightBox }) {
                     <div key={result.collection_id} className="card mb-1" onMouseOver={() => setHighlightBox({ id: result.collection_id, title: result.metadata.title, bbox: result.metadata.bounding_box })}>
                         <div className="card-header text-truncate">
                             <Link target="_blank" className="stretched-link" title={result.metadata.title} to={"/item/" + result.collection_id}>{result.metadata.title}</Link>
-                            <p className="card-text">{result.metadata.year} - {result.metadata.authors.map(author => author.person).join(' ')}</p>
+                            <p className="card-text">{result.metadata.year} - {result.metadata.authors & result.metadata.authors.length > 0 ? 
+                                result.metadata.authors.map(author => author.person).join(' ') :
+                                "No author specified"
+                            }</p>
                         </div>
                     </div>
                 )
