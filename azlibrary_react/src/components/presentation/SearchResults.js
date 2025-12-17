c8d783c3ad3356baeac608b59d6ce9e970c0c040
Douglas Meredith <noisyfloweres@douglasmeredith.net>
2025-12-17 16:14:23 -0700

    Only display author.person. Display different messages for no authors vs author with not name

--- azlibrary_react/src/components/presentation/SearchResults.js
+++ azlibrary_react/src/components/presentation/SearchResults.js
@@ -13,15 +13,9 @@ export default function SearchResults({ results, setHighlightBox }) {
                             <Link target="_blank" className="stretched-link" title={result.metadata.title} to={"/item/" + result.collection_id}>{result.metadata.title}</Link>
                             <p className="card-text">{result.metadata.year} - {result.metadata.authors && result.metadata.authors.length > 0 ? 
                                 result.metadata.authors.map(author => 
-                                    author.person ?
-                                        author.person :
-                                        author.surname ?
-                                            author.surname + (author.givenname ? `, ${author.givenname}` : '') :
-                                            author.organization ?
-                                                author.organization :
-                                                "No author specified"
+                                    author.person || "Author name not specified"
                                 ).join(' ') :
-                                "No author specified"
+                                "No authors listed"
                             }</p>
                         </div>
                     </div>
